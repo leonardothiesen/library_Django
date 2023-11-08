2025c8c71cbe76a6caf8b1cc92059b396e8d2193
leonardothiesen <127902767+leonardothiesen@users.noreply.github.com>
2023-11-07 21:16:42 -0300

    codificando e conferindo todos os arquivos

---
+++ library_ads/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('inicio/', views.index, name='home'),
+    path('book-detail/<int:id>/', views.book_detail , name='book-detail')
+
+]
