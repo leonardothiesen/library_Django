b63e40c34a97a0eca420947a1f754b46f97e669c
leonardothiesen <127902767+leonardothiesen@users.noreply.github.com>
2023-11-20 21:29:38 -0300

    subindo

--- library_ads/admin.py
+++ library_ads/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Books, Genres
 
-# Register your models here.
+class BooksAdmin(admin.ModelAdmin):
+    list_display = ['name','genre','pages','cover','author','copies']
+    list_filter = ['name','genre','author']
+    search_fields = ['name','genre','author']
+
+admin.site.register(Books, BooksAdmin)
+admin.site.register(Genres)
--- library_ads/models.py
+++ library_ads/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Genres(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Genre'
+        verbose_name_plural = 'Genres'
+
+class Books(models.Model):
+    
+    name = models.CharField(max_length=255)
+    genre = models.ForeignKey(Genres, on_delete=models.CASCADE) 
+    pages = models.IntegerField()
+    cover = models.ImageField(blank=False)
+    author = models.CharField(max_length=255)
+    copies = models.IntegerField()
+    cod = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Book'
+        verbose_name_plural = 'Books'
