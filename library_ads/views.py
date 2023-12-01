1f62f0c5b5ff2a29d43c32182833c4ae0dd2fbf1
leonardothiesen <127902767+leonardothiesen@users.noreply.github.com>
2023-12-01 20:57:09 -0300

    Emprestimo, devolução, qtde de emprestimo e de devolução ajustador e testados

--- library_ads/views.py
+++ library_ads/views.py
@@ -47,19 +47,15 @@ def search_book (request):
     books = Books.objects.filter(name__icontains=q)
     return render(request, 'pages/index.html', {'books':books})
 
-
-
-
 def burrow_book(request, id):
     if request.method == 'POST':
-        book = Books.objects.get(pk=id)  # Alteração aqui, substituindo book_id por id
+        book = Books.objects.get(pk=id)
         borrow_quantity = int(request.POST.get('borrow_quantity', 0))
 
         if borrow_quantity > 0:
             book.borrow(borrow_quantity)
             return redirect('book_detail', id=id)
         else:
-            # Aqui você pode adicionar lógica para lidar com quantidades inválidas, se necessário
             return HttpResponse("Quantidade inválida")
 
     return HttpResponse("Método não permitido")
@@ -70,33 +66,16 @@ def return_book(request, id):
 
     if request.method == 'POST':
         return_quantity = int(request.POST.get('return_quantity', 1))
-
-        # Verifique se o formulário é válido
         form = ReturnBookForm(request.POST)
         if form.is_valid():
-            # Verifique se a quantidade para devolução é válida
             if 1 <= return_quantity <= book.borrowed:
                 book.stock += return_quantity
                 book.borrowed -= return_quantity
                 book.save()
                 return redirect('home')
             else:
-                # Lógica para lidar com entrada inválida (pode adicionar uma mensagem de erro)
                 return HttpResponse("Quantidade inválida")
         else:
-            # Se o formulário não for válido, você pode adicionar uma lógica para tratamento
             pass
     else:
-        # Lógica para lidar com solicitações não-POST, se necessário
         pass
-
-# def burrow_book(request, id):
-#     book = Books.objects.get(id=id)
-#     book.burrow()
-#     return redirect('home')
-
-# def loan_book(request, id):
-#     book = Books.objects.get(id=id)
-#     book.qtd -= 1 
-#     book.save()
-#     return redirect('book-detail', id)
